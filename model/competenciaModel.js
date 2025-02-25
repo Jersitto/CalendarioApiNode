28768980f10e80c7c3c8255bb16fc184691df167
Bayer96 <juan_drestrepob@soy.sena.edu.co>
2025-02-24 23:40:20 -0500

    agregamos tabla competencia

--- model/competenciaModel.js
+++ model/competenciaModel.js
@@ -15,6 +15,63 @@ class CompetenciaModel {
             callback(null, response);
         });
     }
+
+    //metodo para obtener todas los competencias
+    static obtenerTodos(callback) {
+        const query = 'SELECT * FROM competencia';
+        conn.query(query, (err, response) =>{
+            if(err){
+                callback(err, null);
+            }
+            callback(null, response);
+        });
+    }
+
+    //metodo para para obtener una competencia por su nombre
+    static obtenerPorNombre(nombre, callback){
+        const sql = `SELECT * FROM competencia WHERE nombreCompetencia LIKE '%${nombre}%'`;
+        conn.query(sql, (err, results) => {
+            if (err) {
+                callback(err, null);
+                return;
+            }
+            callback(null, results);
+        });
+    }
+
+    //metodo para insertar una competencia
+    static insertarCompetencia(competencia, callback){
+        const query = 'INSERT INTO competencia SET?';
+        conn.query(query, [competencia], (err, response) =>{
+            if(err){
+                callback(err, null);
+            }
+            callback(null, response);
+        });
+    }
+
+    //metodo para actualizar una competencia
+    static actualizarCompetencia(id, competencia, callback){
+        const query = 'UPDATE competencia SET? WHERE idCompetencia =?';
+        conn.query(query, [competencia, id], (err, response) =>{
+            if(err){
+                callback(err, null);
+            }
+            callback(null, response);
+        });
+    }
+
+    //metodo para eliminar una competencia
+    static eliminarCompetencia(id, callback){
+        const query = 'DELETE FROM competencia WHERE idCompetencia =?';
+        conn.query(query, [id], (err, response) =>{
+            if(err){
+                callback(err, null);
+            }
+            callback(null, response);
+        });
+    }
+
 }
 
 module.exports = CompetenciaModel;
